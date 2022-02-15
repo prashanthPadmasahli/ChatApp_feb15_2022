e66588574d39422d622d375758f161ecf3d9793d
Prashanth <prashanthkumarshettigar@gamil.com>
2022-02-15 12:19:02 +0530

    Initial Commit

---
+++ ChatApp_feb15_2022/ChatApp_feb15_2022App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp_feb15_2022App.swift
+//  ChatApp_feb15_2022
+//
+//  Created by Mac on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp_feb15_2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
